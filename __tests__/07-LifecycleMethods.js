551958d9acf1a0cc967bb849f2ef2e6823baabac
Larry Price <lrp@illinoisrocstar.com>
2018-05-04 08:47:32 -0500

    The last one

--- __tests__/07-LifecycleMethods.js
+++ __tests__/07-LifecycleMethods.js
@@ -1,44 +1,45 @@
-import LifecycleMethodsComponent from '../src/07-LifecycleMethods.js'
+import React from 'react'
+import {shallow, configure} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 import sinon from 'sinon'
+import LifecycleMethodsComponent from '../exercises/07-LifecycleMethods'
+
+configure({adapter: new Adapter()})
 
 describe('07 - Lifecycle methods', () => {
-  let component
+  beforeEach(() => {
+    sinon.spy(console, 'log')
+  })
+
+  afterEach(() => {
+    console.log.restore()
+  })
 
   describe('Task #1 - emit a console log when the component mounts', () => {
-    it("should emit 'I'm mounted!' in developer's console", () => {
-      const mock = sinon.mock(console)
-      mock.expects('log').once().withArgs("I'm mounted!")
+    it('should emit \'I\'m mounted!\' in developer console', () => {
+      shallow(<LifecycleMethodsComponent />)
 
-      const rootNode = document.body.appendChild(document.createElement('div'))
-      React.render(React.createElement(LifecycleMethodsComponent), rootNode)
-      mock.verify()
+      expect(console.log.calledOnce).toBe(true)
+      expect(console.log.getCall(0).args[0]).toBe('I\'m mounted!')
     })
   })
 
   describe('Task #2 - emit a console log when the component updates', () => {
-    it("should emit 'Updated!' in developer's console", () => {
-      const rootNode = document.body.appendChild(document.createElement('div'))
-      const component = React.render(React.createElement(LifecycleMethodsComponent), rootNode)
+    it('should emit \'Updated!\' in developer console', () => {
+      const wrapper = shallow(<LifecycleMethodsComponent />)
+      wrapper.setState({name: 'Victor'})
 
-      const mock = sinon.mock(console)
-      mock.expects('log').exactly(2).withArgs('Updated!')
-
-      component.setState({name: 'Victor'})
-      component.forceUpdate()
-
-      mock.verify()
+      expect(console.log.calledTwice).toBe(true)
+      expect(console.log.getCall(1).args[0]).toBe('Updated!')
     })
   })
 
   describe('Task #3 - emit a console log when the component unmounts', () => {
-    it("should emit 'Goodbye, cruel world! :(' in developer's console", () => {
-      const rootNode = document.body.appendChild(document.createElement('div'))
-      const component = React.render(React.createElement(LifecycleMethodsComponent), rootNode)
+    it('should emit \'Goodbye, cruel world! :(\' in developer console', () => {
+      shallow(<LifecycleMethodsComponent />).unmount()
 
-      const mock = sinon.mock(console)
-      mock.expects('log').once().withArgs('Goodbye, cruel world! :(')
-      React.unmountComponentAtNode(rootNode)
-      mock.verify()
+      expect(console.log.calledTwice).toBe(true)
+      expect(console.log.getCall(1).args[0]).toBe('Goodbye, cruel world! :(')
     })
   })
 })
